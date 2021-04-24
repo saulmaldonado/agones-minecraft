fa8f29d3fe0a56f56091aacc8df6d1726a52f9c2
Saul Maldonado <samaldonado4@gmail.com>
2021-04-24 01:59:46 -0500

    feat: Add cloudDNS client to reconcile
    cloudDNS client will create A and SRV records on GCP for every
    GameServer made

--- controller/go.mod
+++ controller/go.mod
@@ -4,6 +4,11 @@ go 1.16
 
 require (
 	agones.dev/agones v1.13.0
+	github.com/go-logr/logr v0.3.0
+	github.com/smartystreets/assertions v1.0.1 // indirect
+	golang.org/x/oauth2 v0.0.0-20210413134643-5e61552d6c78
+	google.golang.org/api v0.45.0
+	gopkg.in/ini.v1 v1.51.1 // indirect
 	k8s.io/apimachinery v0.20.2
 	k8s.io/client-go v0.20.2
 	sigs.k8s.io/controller-runtime v0.8.3
--- controller/main.go
+++ controller/main.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"log"
+	"os"
+	"sync"
 
 	agonesv1 "agones.dev/agones/pkg/apis/agones/v1"
+	"github.com/go-logr/logr"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/dns/v1"
+	"google.golang.org/api/option"
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	clientGoScheme "k8s.io/client-go/kubernetes/scheme"
@@ -13,38 +20,89 @@ import (
 	controller "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
 
 type GameServerReconciler struct {
 	client.Client
-	Scheme *runtime.Scheme
+	Scheme    *runtime.Scheme
+	log       logr.Logger
+	dns       *dns.Service
+	cfg       *GoogleConfig
+	endpoints map[string]bool
+	lock      sync.Mutex
+}
+
+type GoogleConfig struct {
+	ProjectId   string
+	ManagedZone string
+}
+
+var (
+	ManagedZone string
+	ProjectID   string
+)
+
+func init() {
+	flag.StringVar(&ManagedZone, "zone", "", "DNS zone that the controller will manage")
+	flag.StringVar(&ProjectID, "project", "", "Google project ID")
+
+	flag.Parse()
 }
 
 func main() {
 	scheme := runtime.NewScheme()
-	agonesv1.AddToScheme(scheme)
-	clientGoScheme.AddToScheme(scheme)
+	log := zap.New()
 
-	log.Println("Setting up controller")
+	if err := agonesv1.AddToScheme(scheme); err != nil {
+		log.Error(err, "Error Adding agones.dev/v1 to scheme")
+	}
+
+	if err := clientGoScheme.AddToScheme(scheme); err != nil {
+		log.Error(err, "Error adding client-go clientset to scheme")
+	}
+
+	log.Info("Initializing DNS client")
+
+	gcloud, err := google.DefaultClient(context.Background(), dns.NdevClouddnsReadwriteScope)
+	if err != nil {
+		log.Error(err, "Error getting credentials")
+	}
+
+	dns, err := dns.NewService(context.Background(), option.WithHTTPClient(gcloud))
+	if err != nil {
+		log.Error(err, "Error getting DNS client")
+	}
+
+	log.Info("Setting up manager")
 
 	manager, err := controller.NewManager(config.GetConfigOrDie(), controller.Options{
 		Scheme: scheme,
 	})
 	if err != nil {
-		log.Fatal(err)
+		log.Error(err, "Error setting up manager")
+		os.Exit(1)
 	}
+
+	log.Info("Setting up controller")
+
+	cfg := GoogleConfig{ProjectID, ManagedZone}
+
+	fmt.Println(cfg)
 
 	err = controller.NewControllerManagedBy(manager).
 		For(&agonesv1.GameServer{}).
-		Complete(&GameServerReconciler{manager.GetClient(), manager.GetScheme()})
+		Complete(&GameServerReconciler{manager.GetClient(), manager.GetScheme(), log, dns, &cfg, map[string]bool{}, sync.Mutex{}})
 	if err != nil {
-		log.Fatal(err)
+		log.Error(err, "Error setting up controller")
+		os.Exit(1)
 	}
 
-	log.Println("Starting manager")
+	log.Info("Starting manager")
 	if err := manager.Start(controller.SetupSignalHandler()); err != nil {
-		log.Fatal(err)
+		log.Error(err, "Error starting manager")
+		os.Exit(1)
 	}
 }
 
@@ -53,7 +111,7 @@ func (r *GameServerReconciler) Reconcile(ctx context.Context, req reconcile.Requ
 
 	err := r.Get(ctx, req.NamespacedName, gs)
 	if errors.IsNotFound(err) {
-		log.Println("could not find GameServer")
+		r.log.Error(err, "could not find GameServer")
 		return reconcile.Result{}, nil
 	}
 
@@ -62,14 +120,85 @@ func (r *GameServerReconciler) Reconcile(ctx context.Context, req reconcile.Requ
 	}
 
 	if gs.Annotations["agones-mc/hostname"] != "" && gs.Annotations["agones-mc/externalDNS"] == "" {
-		log.Println("Adding external DNS")
 		hostname := gs.Annotations["agones-mc/hostname"]
+
+		r.Get(ctx, req.NamespacedName, gs)
+
+		if err := r.setExternalDNS(hostname, gs); err != nil {
+
+			switch err := err.(type) {
+			case *RecordExistsErr:
+				r.log.Info("Record already exists")
+			case *ServerNotReady:
+				r.log.Info("Server not ready")
+			default:
+				return reconcile.Result{}, err
+			}
+
+		}
+
 		gs.Annotations["agones-mc/externalDNS"] = gs.Name + "." + hostname
 		if err := r.Update(ctx, gs); err != nil {
 			return reconcile.Result{}, err
 		}
-		log.Println("GameServer updated")
+		r.log.Info("GameServer updated")
 	}
 
 	return reconcile.Result{}, nil
+}
+
+func (r *GameServerReconciler) setExternalDNS(hostname string, gs *agonesv1.GameServer) error {
+	if gs.Status.Address == "" || len(gs.Status.Ports) == 0 {
+		return &ServerNotReady{}
+	}
+
+	hostExternalIp := gs.Status.Address
+	port := gs.Status.Ports[0].Port
+
+	r.lock.Lock()
+	defer r.lock.Unlock()
+
+	recordName := fmt.Sprintf("%s.%s.", gs.Name, hostname)
+	srvRecordName := fmt.Sprintf("_minecraft._tcp.%s", recordName)
+
+	if _, ok := r.endpoints[recordName]; ok {
+		return &RecordExistsErr{recordName}
+	}
+
+	change := dns.Change{}
+
+	srvResourceRecord := fmt.Sprintf("0 1 %d %s", port, recordName)
+
+	srvRecord := dns.ResourceRecordSet{Type: "SRV", Name: srvRecordName, Rrdatas: []string{srvResourceRecord}, Ttl: 60 * 30}
+	aRecord := dns.ResourceRecordSet{Type: "A", Name: recordName, Rrdatas: []string{hostExternalIp}, Ttl: 60 * 30}
+
+	change.Additions = []*dns.ResourceRecordSet{&aRecord, &srvRecord}
+
+	res, err := r.dns.Changes.Create(r.cfg.ProjectId, r.cfg.ManagedZone, &change).Do()
+
+	if err != nil {
+		return err
+	}
+
+	r.endpoints[recordName] = true
+
+	json, _ := res.MarshalJSON()
+
+	log.Println(string(json))
+
+	return nil
+}
+
+type RecordExistsErr struct {
+	record string
+}
+
+func (e *RecordExistsErr) Error() string {
+	return fmt.Sprintf("Record %s already exists", e.record)
+}
+
+type ServerNotReady struct{}
+
+func (e *ServerNotReady) Error() string {
+	return "Server IP and port not allocated"
 }
